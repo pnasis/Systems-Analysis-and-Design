7c7715ce750605aa280328260566edcbd47913a1
Dimitris Kroustallis <dkroustalis1@gmail.com>
2022-06-07 21:10:51 +0300

    Update Member.java

--- Member.java
+++ Member.java
@@ -3,18 +3,19 @@ public class Member extends User {
 	private String name;
 	private String signature;
 	private Application application;
-
+	
+	
+	public Member(String email, String password)
+	{
+		super();
+	}
+	
 	public String getName() {
 		return this.name;
 	}
 
-	/**
-	 * 
-	 * @param vote
-	 * @param comment
-	 */
 	public void vote(boolean vote, String comment) {
-		// TODO - implement Members.vote
+		
 		throw new UnsupportedOperationException();
 	}
 
