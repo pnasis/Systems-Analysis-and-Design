dbe7e9ad3e95aa709e9cf79799d2432cd5347bfa
Marios Leontief <105859565+leomarios@users.noreply.github.com>
2022-06-09 12:28:47 +0300

    Changes made to method "vote"

--- Member.java
+++ Member.java
@@ -2,23 +2,34 @@ public class Member extends User {
 
 	private String name;
 	private String signature;
-	private Application application;
+	private ArrayList<Application> applications;
 	
 	
-	public Member(String email, String password, String name, String signature)
+	public Member(String email, String password, String name, String signature, 
+		      ArrayList<Application> list)
 	{
-		super();// To eixa super(email,signature) kai moy ebgaze error
+		
+		super(email,password);// To eixa super(email,signature) kai moy ebgaze error
 		this.name = name;
 		this.signature = signature;
+		this.applications = list;
 	}
 	
 	public String getName() {
 		return this.name;
 	}
 
-	public void vote(boolean vote, String comment) {
+	public void vote(boolean vote, String comment, Application app) {
+		Decision aDecision;
 		
-		throw new UnsupportedOperationException();
+		for(Application a : application) {
+			if(a.getId() == app.getId()) {
+				aDecision = a.getDecision();
+				aDecision.addVote(vote);
+				aDecision.addComments(comment);
+				a.setDecision(aDecision); 
+			}
+		}
 	}
 	
 
